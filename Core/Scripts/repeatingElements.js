e20941cdb8323baf145a9e5eb4267ce63a763b03
Alexander15013 <bhm351@myy.haaga-helia.fi>
2026-08-27 23:18:28 +0300

    Ver 0.05.3

--- Core/Scripts/repeatingElements.js
+++ Core/Scripts/repeatingElements.js
@@ -1,14 +1,34 @@
+let headerIndex = document.getElementById('signatureMenuIndex');
+if (headerIndex) {
+headerIndex.innerHTML = `
+  <h2>
+    <a href="index.html"><span class="textWhite">Alexander Soprych</span></a> 
+    <a href="index.html"><span class="textBlack">Alexander Soprych</span></a>
+  </h2>
+  <div>
+    <h2 class="navMenu">
+      <a href="index.html"><span class="textWhite">Projects</span></a>
+      <a href="index.html"><span class="textBlack">Projects</span></a>
+    </h2>
+    <h2 class="navMenu">
+      <a href="Core/Pages/About_Me.html"><span class="textWhite">About</span></a>
+      <a href="Core/Pages/About_Me.html"><span class="textBlack">About</span></a>
+    </h2>
+  </div>
+`;
+}
+
 let header = document.getElementById('signatureMenu');
 if (header) {
 header.innerHTML = `
   <h2>
-    <a href="../Pages/index.html"><span class="textWhite">Alexander Soprych</span></a> 
-    <a href="../Pages/index.html"><span class="textBlack">Alexander Soprych</span></a>
+    <a href="../../index.html"><span class="textWhite">Alexander Soprych</span></a> 
+    <a href="../../index.html"><span class="textBlack">Alexander Soprych</span></a>
   </h2>
   <div>
     <h2 class="navMenu">
-      <a href="../Pages/index.html"><span class="textWhite">Projects</span></a>
-      <a href="../Pages/index.html"><span class="textBlack">Projects</span></a>
+      <a href="../../index.html"><span class="textWhite">Projects</span></a>
+      <a href="../../index.html"><span class="textBlack">Projects</span></a>
     </h2>
     <h2 class="navMenu">
       <a href="../Pages/About_Me.html"><span class="textWhite">About</span></a>
