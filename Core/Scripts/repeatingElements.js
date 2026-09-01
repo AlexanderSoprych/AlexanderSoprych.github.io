3c5ed0a2979c42a3a63e54d808dc39666b1f8f5f
Alexander15013 <bhm351@myy.haaga-helia.fi>
2026-09-01 21:48:31 +0300

    Ver 1.0

--- Core/Scripts/repeatingElements.js
+++ Core/Scripts/repeatingElements.js
@@ -7,8 +7,8 @@ headerIndex.innerHTML = `
   </h2>
   <div>
     <h2 class="navMenu">
-      <a href="index.html"><span class="textWhite">Projects</span></a>
-      <a href="index.html"><span class="textBlack">Projects</span></a>
+      <a href="Core/Pages/Projects.html"><span class="textWhite">Projects</span></a>
+      <a href="Core/Pages/Projects.html"><span class="textBlack">Projects</span></a>
     </h2>
     <h2 class="navMenu">
       <a href="Core/Pages/About_Me.html"><span class="textWhite">About</span></a>
@@ -27,12 +27,29 @@ header.innerHTML = `
   </h2>
   <div>
     <h2 class="navMenu">
-      <a href="../../index.html"><span class="textWhite">Projects</span></a>
-      <a href="../../index.html"><span class="textBlack">Projects</span></a>
+      <a href="../Pages/Projects.html"><span class="textWhite">Projects</span></a>
+      <a href="../Pages/Projects.html"><span class="textBlack">Projects</span></a>
     </h2>
     <h2 class="navMenu">
       <a href="../Pages/About_Me.html"><span class="textWhite">About</span></a>
       <a href="../Pages/About_Me.html"><span class="textBlack">About</span></a>
+    </h2>
+  </div>
+`;
+}
+
+let headerWhite = document.getElementById('signatureMenuWhite');
+if (headerWhite) {
+headerWhite.innerHTML = `
+  <h2>
+    <a href="../../index.html"><span class="textBaseWhite">Alexander Soprych</span></a> 
+  </h2>
+  <div>
+    <h2 class="navMenu">
+      <a href="../Pages/Projects.html"><span class="textBaseWhite">Projects</span></a>
+    </h2>
+    <h2 class="navMenu">
+      <a href="../Pages/About_Me.html"><span class="textBaseWhite">About</span></a>
     </h2>
   </div>
 `;
